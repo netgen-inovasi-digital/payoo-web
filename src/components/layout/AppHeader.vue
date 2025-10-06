41ef04eac49166066df7b32a151fddf94cff2b36
ridhalesmana <ridhalesmana07@gmail.com>
2025-10-06 15:01:20 +0800

    update

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -74,8 +74,6 @@
         class="items-center justify-between w-full gap-4 px-5 py-4 shadow-theme-md lg:flex lg:justify-end lg:px-0 lg:shadow-none"
       >
         <div class="flex items-center gap-2 2xsm:gap-3" style="display: none">
-          <!-- <ThemeToggler /> -->
-          <NotificationMenu />
         </div>
         <UserMenu />
       </div>
@@ -86,10 +84,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useSidebar } from '@/composables/useSidebar'
-import ThemeToggler from '../common/ThemeToggler.vue'
-import SearchBar from './header/SearchBar.vue'
+
 import HeaderLogo from './header/HeaderLogo.vue'
-import NotificationMenu from './header/NotificationMenu.vue'
 import UserMenu from './header/UserMenu.vue'
 
 const { toggleSidebar, toggleMobileSidebar, isMobileOpen } = useSidebar()
@@ -102,13 +98,13 @@ const handleToggle = () => {
   }
 }
 
-const dropdownOpen = ref(false)
-const notifying = ref(false)
+// const dropdownOpen = ref(false)
+// const notifying = ref(false)
 
-const toggleDropdown = () => {
-  dropdownOpen.value = !dropdownOpen.value
-  notifying.value = false
-}
+// const toggleDropdown = () => {
+//   dropdownOpen.value = !dropdownOpen.value
+//   notifying.value = false
+// }
 
 const isApplicationMenuOpen = ref(false)
 
--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -9,17 +9,10 @@
       'lg:translate-x-0': true,
     },
   ]" @mouseenter="!isExpanded && (isHovered = true)" @mouseleave="isHovered = false">
-    <div :class="[
-      'py-8 flex',
-      !isExpanded && !isHovered ? 'lg:justify-center' : 'justify-start',
-    ]">
+   <div class="py-8 flex justify-center">
       <router-link to="/">
-        <!-- <img v-if="isExpanded || isHovered || isMobileOpen" class="dark:hidden" src="/images/logo/logo.svg" alt="Logo"
-          width="150" height="40" />
-        <img v-if="isExpanded || isHovered || isMobileOpen" class="hidden dark:block" src="/images/logo/logo-dark.svg"
-          alt="Logo" width="150" height="40" />
-        <img v-else src="/images/logo/logo-icon.svg" alt="Logo" width="32" height="32" /> -->
-        PAYOO
+        <img v-if="isExpanded || isHovered || isMobileOpen" class="dark:hidden" src="/images/logo/Logo Payoo.png" alt="Logo"
+          width="120" height="100" />
       </router-link>
     </div>
     <div class="flex flex-col overflow-y-auto duration-300 ease-linear no-scrollbar">
